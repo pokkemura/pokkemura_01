bf8024c8819dd5f08514eca84341aac3f7afb756
Asa563 <asahi.y.ivi@gmail.com>
2017-11-07 10:08:56 +0900

    aa
    aa

---
+++ sanshoku/sanshoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sanshoku
+//
+//  Created by tsit.st2 on 2017/10/30.
+//  Copyright © 2017年 Asahi Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
